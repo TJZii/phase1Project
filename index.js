a0bb7d1fcf226185187158d4813c6d583392fe40
ziitania <tjhaweszii@gmail.com>
2021-04-11 12:47:30 -0700

    removes the ability for repeated appearances until all options have been cycled through

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const artray = ["https://static.wikia.nocookie.net/mrfz/images/7/7f/Exusiai_Elite_2.png/revision/latest?cb=20190623025156", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_112_siege_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_134_ifrit_2.png", "https://i.redd.it/w1kqqskuihd41.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_202_demkni_2.png", "https://static.wikia.nocookie.net/mrfz/images/8/82/Nightingale_Elite_2.png/revision/latest?cb=20190724040610", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_180_amgoat_2.png", "https://static.wikia.nocookie.net/mrfz/images/e/ee/Ch%27en_Elite_2.png/revision/latest?cb=20190713060136", "https://static.wikia.nocookie.net/mrfz/images/6/6d/SilverAsh_Elite_2.png/revision/latest?cb=20190628063249", "https://static.wikia.nocookie.net/mrfz/images/a/af/Shining_Elite_2.png/revision/latest?cb=20190724040755", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_204_platnm_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_136_hsguma_2.png", "https://static.wikia.nocookie.net/mrfz/images/2/2d/Skyfire_Elite_2.png/revision/latest?cb=20190724204053", "https://static.wikia.nocookie.net/mrfz/images/7/71/Specter_Elite_2.png/revision/latest?cb=20190724204318", "https://static.wikia.nocookie.net/mrfz/images/0/01/Indra_Elite_2.png/revision/latest?cb=20190724032342", "https://static.wikia.nocookie.net/mrfz/images/1/11/Meteorite_Elite_2.png/revision/latest?cb=20190724034944", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_241_panda_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2020-01/ArtCroissantE2.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_215_mantic_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_002_amiya_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_145_prove_2.png", "https://static.wikia.nocookie.net/mrfz/images/1/18/Silence_Elite_2.png/revision/latest?cb=20190625063651", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_106_franka_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_171_bldsk_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2020-02/ArtCliffheartSkinWild.png", "https://static.wikia.nocookie.net/mrfz/images/6/6c/Zima_Elite_2.png/revision/latest?cb=20190817203454", "https://gamepress.gg/arknights/sites/arknights/files/2020-06/char_195_glassb_kitchen%231.png", "https://static.wikia.nocookie.net/mrfz/images/1/11/Firewatch_Elite_2.png/revision/latest?cb=20190724025420", "https://gachax.com/arknights/wp-content/uploads/sites/6/2020/02/pramanix-winter-messenger-image.png", "https://static.wikia.nocookie.net/mrfz/images/9/91/Texas_Elite_2.png/revision/latest?cb=20190621194815", "https://static.wikia.nocookie.net/mrfz/images/9/91/Ptilopsis_Elite_2.png/revision/latest?cb=20190625063652", "https://static.wikia.nocookie.net/mrfz/images/2/23/Mayer_Elite_2.png/revision/latest?cb=20190625063656", "https://static.wikia.nocookie.net/mrfz/images/1/1c/Vulcan_Elite_2.png/revision/latest?cb=20190713060426", "https://gamepress.gg/arknights/sites/arknights/files/2020-05/char_140_whitew_boc1.png", "https://static.wikia.nocookie.net/mrfz/images/6/69/Liskarm_Elite_2.png/revision/latest?cb=20190627031239", "https://static.wikia.nocookie.net/mrfz/images/7/7f/Blue_Poison_Elite_2.png/revision/latest?cb=20190620082455", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_235_jesica_2.png", "https://static.wikia.nocookie.net/mrfz/images/2/28/Projekt_Red_Elite_2.png/revision/latest?cb=20190724201633", "https://static.wikia.nocookie.net/mrfz/images/c/ca/Nearl_Elite_2.png/revision/latest?cb=20190724040514", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_101_sora_1.png", "https://static.wikia.nocookie.net/mrfz/images/d/d3/Savage_Elite_2.png/revision/latest?cb=20190724202535", "https://static.wikia.nocookie.net/mrfz/images/5/5c/Cuora_Elite_2.png/revision/latest?cb=20190723210803", "https://static.wikia.nocookie.net/mrfz/images/7/72/Gavial_Elite_2.png/revision/latest?cb=20190724030007", "https://static.wikia.nocookie.net/mrfz/images/2/20/Gummy_Elite_2.png/revision/latest?cb=20190817203453", "https://static.wikia.nocookie.net/mrfz/images/2/2e/Estelle_Elite_2.png/revision/latest?cb=20190724014449", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_117_myrrh_2.png", "https://static.wikia.nocookie.net/mrfz/images/9/93/Dobermann_Elite_2.png/revision/latest?cb=20190723211557", "https://static.wikia.nocookie.net/mrfz/images/9/94/Matterhorn_Elite_2.png/revision/latest?cb=20190628063251", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_110_deepcl_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_185_frncat_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_149_scave_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_289_gyuki_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_183_skgoat_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_181_flower_2.png", "https://static.wikia.nocookie.net/mrfz/images/c/c3/Haze_Elite_2.png/revision/latest?cb=20190724031216", "https://static.wikia.nocookie.net/mrfz/images/8/8c/Gravel_Elite_2.png/revision/latest?cb=20190627031445", "https://static.wikia.nocookie.net/mrfz/images/8/8e/Vigna_Elite_2.png/revision/latest?cb=20190724204741", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_193_frostl_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_236_rope_2.png", "https://static.wikia.nocookie.net/mrfz/images/d/da/Gitano_Elite_2.png/revision/latest?cb=20190724030431", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_198_blackd_2.png", "https://gamepress.gg/arknights/sites/arknights/files/2020-02/ArtHibiscusSkinDark_0.png", "https://static.wikia.nocookie.net/mrfz/images/4/4f/ShiraYuki_Elite_2.png/revision/latest?cb=20190627031443", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_277_sqrrel_1.png", "https://gamepress.gg/arknights/sites/arknights/files/2020-02/ArtMelanthaSkinEpoque.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_278_orchid_1.png", "https://static.wikia.nocookie.net/mrfz/images/a/ae/Meteor_Elite_2.png/revision/latest?cb=20190724034538", "https://gamepress.gg/arknights/sites/arknights/files/2020-01/char_123_fang_winter%231.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_210_stward_1.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_212_ansel_1.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_209_ardign_1.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_211_adnach_1.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_009_12fce_1.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_240_wyvern_1_0.png", "https://static.wikia.nocookie.net/mrfz/images/4/49/Durin.png/revision/latest?cb=20190620082558", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_192_falco_1.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_500_noirc_1.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_502_nblade_1.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-11/char_124_kroos_1_0.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_121_lava_1.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_286_cast3_1.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_503_rang_1.png", "https://gamepress.gg/arknights/sites/arknights/files/2020-05/ArtBeagleSkinFearless.png", "https://gamepress.gg/arknights/sites/arknights/files/2019-10/char_285_medic2_1.png"];
 let squadTracker = 0;
+let chosenSquad = [];
+let selectedOps = [];
 
 
 const init = () => {
@@ -12,7 +14,10 @@ const init = () => {
     const squadMaker = document.getElementById('squadAdder');
 
     squadMaker.addEventListener(`click`, function() {
-        addToCurrentSquad();
+        if(chosenSquad.includes(document.getElementById(`name`).innerText) == false){
+            chosenSquad.push(document.getElementById(`name`).innerText);
+            addToCurrentSquad();
+        }
     });
 
     testerPresser.addEventListener(`click`, function() {
@@ -27,6 +32,10 @@ const init = () => {
             console.log(operators);
 
             let randOpSelect = Math.floor(Math.random()*operators.length);
+            
+            while(selectedOps.includes(randOpSelect)){
+                randOpSelect = Math.floor(Math.random()*operators.length);
+            }
 
             document.getElementById(`name`).innerText = `${operators[randOpSelect].name}`;
             classIconSwapper(operators[randOpSelect].class);
@@ -36,6 +45,13 @@ const init = () => {
             document.getElementById(`faction`).innerText = `${operators[randOpSelect].faction}`;
             document.getElementById(`rarity`).innerText = `${operators[randOpSelect].rarity} Stars`;
             document.getElementById(`artwork`).src = artray[randOpSelect];
+            selectedOps.push(randOpSelect);
+
+            console.log(selectedOps);
+
+            if(selectedOps.length == operators.length){
+                selectedOps.splice(0, selectedOps.length);
+            }
 
         });
     }
@@ -130,5 +146,4 @@ const init = () => {
 
 }
 
-document.addEventListener('DOMContentLoaded', init)
-
+document.addEventListener('DOMContentLoaded', init);
